a91b1389c63299825cadad31774e72c6e2751655
Whitney Hiemstra <whitcolorado@gmail.com>
2015-05-19 23:30:50 -0600

    Added Update/Edit and Delete functions

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -3,6 +3,7 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -25,5 +26,24 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id' do |id|
     @task = TaskManager.find(id.to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do |id|  #any time you use the : (a string prefaced by a :) not a symbol
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id| #captured the id form the URL
+    TaskManager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
+  end
+
+  not_found do
+    erb :error
   end
 end
--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -25,11 +25,27 @@ class TaskManager
     raw_tasks.map { |data| Task.new(data) }
   end
 
-  def self. raw_task(id)
+  def self.raw_task(id)
     raw_tasks.find { |task| task["id"] == id}
   end
 
   def self.find(id)
     Task.new(raw_task(id))
+  end
+
+  def self.update(id, data) #Schoolwork
+    database.transaction do
+      target = database['tasks'].find { |task| task['id'] == id }
+      # target = {"title => "Homework", "description" => "math" } -- it's grabbing the task by it's id and giving that task/description back to us
+
+      target['title'] = data[:title] #resetting the previous task title of "Homework" to "Schoolwork"
+      target['description'] = data[:description]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database['tasks'].delete_if { |task| task['id'] == id }
+    end
   end
 end
